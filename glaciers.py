0583e73c9bc7204b44074234d95b8fe29c626d95
Xenofon-Chiotopoulos <92038142+Xenofon-Chiotopoulos@users.noreply.github.com>
2021-11-21 23:40:24 +0000

    __init__ glacier object with validation for inupts

--- glaciers.py
+++ glaciers.py
@@ -12,8 +12,42 @@ file_path_EE = Path(location + "sheet-EE.csv")
 
 class Glacier:
     def __init__(self, glacier_id, name, unit, lat, lon, code):
-        #testing
-        raise NotImplementedError
+        self.glacier_id = glacier_id
+        if type(glacier_id) != str:
+            raise TypeError('incorrect type. Enter a string')
+        if len(glacier_id) != 5:
+            raise Exception('Id not appropriate length. Enter a string of length 5')
+
+        self.name = name
+        if type(name) != str:
+            raise TypeError('incorrect type. Enter a string')
+
+        self.unit = unit
+        if type(unit) != str:
+            raise TypeError('incorrect type. Enter a string')
+        if len(unit) != 2:
+            raise Exception('Id not appropriate length. Enter a string of length 2')
+
+        self.lat = lat
+        if type(lat) != int and type(lat) != float:
+            raise TypeError('incorrect type. Enter a integer or float')
+        if float(lat) < -90 or float(lat) > 90:
+            raise ValueError('latitude input is out of bounds. Latitude must be between -90 and 90')
+
+        self.lon = lon
+        if type(lon) != int and type(lon) != float:
+            raise TypeError('incorrect type. Enter a integer or float')
+        if float(lon) < -180 or float(lon) > 180:
+            raise ValueError('latitude input is out of bounds. Latitude must be between -180 and 180')
+
+        self.code = code
+        if len(str(code)) != 3:
+            raise Exception('Input of wrong length. Enter 3 digit code')
+
+        self.current_year_balance = 0
+        self.year = []
+        self.mass_balance = []
+        self.year_balance_dict = {}
 
     def add_mass_balance_measurement(self, year, mass_balance):
         raise NotImplementedError
